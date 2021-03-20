2802dbff1f9b945b9bee2ad765d7b413a221a948
unknown <marouanemiramax4@gmail.com>
2021-03-19 21:07:09 -0300

    modulo

--- blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/model/postagem.java
+++ blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/model/postagem.java
@@ -1,4 +1,5 @@
 package org.generation.blogpessoal.model;
+
 import java.util.*;
 
 import javax.persistence.Entity;
@@ -7,30 +8,40 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 @Entity
 @Table(name = "postagem")
 public class postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
+
 	private long id;
-	
+
 	@NotNull
-	
-	@Size ( min=5 , max=100)
-	
+
+	@Size(min = 5, max = 100)
+
 	private String titulo;
-	
+
 	@NotNull
-	@Size ( min = 10 , max = 500)
+	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -63,6 +74,13 @@ public class postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
